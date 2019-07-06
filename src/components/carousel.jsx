8bab20fd12470519846ae2901903e020e7d7a9af
Mitchell <malexanderdill@gmail.com>
2019-07-05 19:53:48 -0500

    Implement image preview in carousel

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -11,39 +11,39 @@ const Carousel = (props) => (
                 <div class="carousel-item active">
                     <div class="row carousel-third">
                         <div class="col">
-                            <img class="d-block w-100" src={props.imageUrl} alt="1 slide"></img>
+                            <img class="d-block w-100" src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-switch/e/ea/Grookey.jpg?width=325" alt="1 slide"></img>
                         </div>
                         <div class="col">
-                            <img class="d-block w-100" src={props.imageUrl} alt="2 slide"></img>
+                            <img class="d-block w-100" src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-switch/4/40/Sobble.jpg?width=1280" alt="2 slide"></img>
                         </div>
                         <div class="col">
-                            <img class="d-block w-100" src={props.imageUrl} alt="3 slide"></img>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="row carousel-third">
-                        <div class="col">
-                            <img class="d-block w-100" src={props.imageUrl} alt="4 slide"></img>
-                        </div>
-                        <div class="col">
-                            <img class="d-block w-100" src={props.imageUrl} alt="5 slide"></img>
-                        </div>
-                        <div class="col">
-                            <img class="d-block w-100" src={props.imageUrl} alt="6 slide"></img>
+                            <img class="d-block w-100" src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-switch/7/7c/Scorbunny.jpg?width=325" alt="3 slide"></img>
                         </div>
                     </div>
                 </div>
                 <div class="carousel-item">
                     <div class="row carousel-third">
                         <div class="col">
-                            <img class="d-block w-100" src={props.imageUrl} alt="7 slide"></img>
+                            <img class="d-block w-100" src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-switch/e/ea/Grookey.jpg?width=325" alt="4 slide"></img>
                         </div>
                         <div class="col">
-                            <img class="d-block w-100" src={props.imageUrl} alt="8 slide"></img>
+                            <img class="d-block w-100" src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-switch/4/40/Sobble.jpg?width=1280" alt="5 slide"></img>
                         </div>
                         <div class="col">
-                            <img class="d-block w-100" src={props.imageUrl} alt="9 slide"></img>
+                            <img class="d-block w-100" src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-switch/7/7c/Scorbunny.jpg?width=325" alt="6 slide"></img>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="row carousel-third">
+                        <div class="col">
+                            <img class="d-block w-100" src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-switch/e/ea/Grookey.jpg?width=325" alt="7 slide"></img>
+                        </div>
+                        <div class="col">
+                            <img class="d-block w-100" src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-switch/4/40/Sobble.jpg?width=1280" alt="8 slide"></img>
+                        </div>
+                        <div class="col">
+                            <img class="d-block w-100" src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-switch/7/7c/Scorbunny.jpg?width=325" alt="9 slide"></img>
                         </div>
                     </div>
                 </div>
@@ -63,3 +63,4 @@ const Carousel = (props) => (
 export default Carousel;
 
       
+{/* <img class="d-block w-100" src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-switch/4/40/Sobble.jpg?width=1280" alt="1 slide"></img> */}
