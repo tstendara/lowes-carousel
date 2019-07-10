f65050b583106d599cf916bf16f0eca10ca08f26
Hack Reactor Student <student@hackreactor.com>
2019-07-10 17:13:33 -0500

    Add Lowe's fonts and icons

--- src/components/slideInfo.jsx
+++ src/components/slideInfo.jsx
@@ -4,7 +4,10 @@ import SaleInfo from "./saleInfo.jsx";
 const SlideInfo = props => (
   <div class="slideInfo">
     <h5>this is an item and it is really cool of course</h5>
-    <h6>STARS from REVIEWS</h6>
+    <h6>
+      <span class="reviewStars">{"\uECE2\uECE2\uECE2\uECE1\uECE0"}</span>
+      <span class="smallText">({Math.floor(Math.random() * 250)})</span>
+    </h6>
     <h6>$$$$</h6>
     {props.sale ? <SaleInfo sale={props.sale} /> : null}
   </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app.jsx";
-import "./stylesheets/custom.css";
 
 ReactDOM.render(<App />, document.getElementById("app"));
