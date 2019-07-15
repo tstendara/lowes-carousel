8a6f54becd467c37d897762d25b68c81789d03bf
Mitchell Dill <malexanderdill@gmail.com>
2019-07-15 12:47:46 -0500

    Fix a couple of small issues before lunch

--- server/index.js
+++ server/index.js
@@ -20,17 +20,17 @@ app.use(async (req, res, next) => {
 });
 
 app.get('/users', async (req, res) => {
+  const item = req.body.item;
   if (!req.cookies.user_session) {
     const sessionId = helpers.randomStringifiedNumberOfLength(8);
-    const user = await db.createUser(Number(sessionId));
-    console.log('the new user info is: ', user);
+    await db.createUser(Number(sessionId));
+    const user = await db.getUser(sessionId);
+    await db.recordView(user.id, item.id);    
     res.cookie('user_session', Number(sessionId)).send('Cookie is set');
   } else {
-    const item = req.body.item;
     const user = await db.getUser(req.cookies.user_session);
-    const viewedItem = await db.recordView(user.id, item.id);
-    console.log('user is: ', user, 'viewed is: ', viewedItem)
-    res.send(viewedItem);
+    await db.recordView(user.id, item.id);
+    res.send();
   }
 });
 
