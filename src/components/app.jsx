e9384b29fe412093ebcd0563cb75f78edb762165
Mitchell Dill <malexanderdill@gmail.com>
2019-07-17 11:29:44 -0500

    Build reviews API call

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -6,6 +6,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      productId: "1",
       carouselNames: [
         "Customers Also Viewed",
         "Related Items",
@@ -16,12 +17,22 @@ class App extends React.Component {
         related: [],
         prevViewed: []
       },
-      productId: "1"
+      reviews: {
+        alsoViewed: [],
+        related: [],
+        prevViewed: []
+      },
+      prices: {
+        alsoViewed: [],
+        related: [],
+        prevViewed: []
+      }
     };
     this.handleClick = this.handleClick.bind(this);
     this.emitProductId = this.emitProductId.bind(this);
     this.updateUserHistory = this.updateUserHistory.bind(this);
     this.getCarousels = this.getCarousels.bind(this);
+    this.getReviews = this.getReviews.bind(this);
     this.renderCarousels = this.renderCarousels.bind(this);
     this.updateProductView = this.updateProductView.bind(this);
   }
@@ -42,7 +53,6 @@ class App extends React.Component {
     console.log('gonna emit: ', clickedId);
     this.emitProductId(clickedId);
   }
-
   
   emitProductId(productId) {
     let product = new CustomEvent('product', {detail: {product_id: productId}})
@@ -63,9 +73,28 @@ class App extends React.Component {
   //   return Axios.get(`http://ec2-18-188-213-241.us-east-2.compute.amazonaws.com/prices/${this.state.productId}`)
   // }
 
-  // getReviews() {
-  //   return Axios.get(`http://ec2-18-225-6-113.us-east-2.compute.amazonaws.com/api/product/`)
-  // }
+  getReviews() {
+    Axios.get(`http://ec2-18-225-6-113.us-east-2.compute.amazonaws.com/api/stats/all`)
+      .then((allReviews) => {
+        const reviews = {};
+        for (let carousel in this.state.carousels) {
+          const arr = [];
+          carousel.forEach((item) => {
+            const reviewData = allReviews.data[item.id - 1].reviewStats;
+            arr.push([reviewData.reviewCount, reviewData.averageStars]);
+          })
+          reviews[carousel] = arr;
+        }
+        return reviews;
+      })
+      .then((applicableReviews) => {
+        this.setState({
+          reviews: applicableReviews
+        });
+        console.log('applic reviews should be: ',applicableReviews);
+      })
+      .catch(err => {console.log(err)})
+  }
   
   renderCarousels(newCarousels) {
     this.setState({
@@ -77,6 +106,7 @@ class App extends React.Component {
     this.setState({productId: newProductId})
     this.updateUserHistory(newProductId)
       .then(this.getCarousels)
+      .then(this.getReviews)
       .then(this.renderCarousels)
       .catch(err => {console.log('event listener says: ', err)})
   }
@@ -87,16 +117,19 @@ class App extends React.Component {
         <Carousel
           name={this.state.carouselNames[0]}
           images={this.state.carousels.alsoViewed.slice(0, 15)}
+          reviews={this.state.reviews.alsoViewed}
           handleClick={this.handleClick}
         />
         <Carousel
           name={this.state.carouselNames[1]}
           images={this.state.carousels.related.slice(0, 15)}
+          reviews={this.state.reviews.related}
           handleClick={this.handleClick}
         />
         <Carousel
           name={this.state.carouselNames[2]}
           images={this.state.carousels.prevViewed.slice(0, 30)}
+          reviews={this.state.reviews.prevViewed}
           handleClick={this.handleClick}
         />
       </div>
