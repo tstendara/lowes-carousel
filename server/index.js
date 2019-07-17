365bf188ceda0ff9445c2ad39d46f042db16f702
Mitchell Dill <malexanderdill@gmail.com>
2019-07-17 17:47:37 -0500

    Set up whitelist

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const db = require("../database/index.js");
 const middleware = require("./middleware.js");
@@ -12,18 +12,40 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cookieParser());
-app.use(cors());
+
+const whitelist = [
+  "http://fec-proxy.us-east-1.elasticbeanstalk.com/",
+  "http://lowesproxy-env.6tim4uzsty.us-east-2.elasticbeanstalk.com/",
+  "http://localhost:3000",
+  "http://fec-lowes-carousel.us-east-2.elasticbeanstalk.com/",
+  "http://ec2-18-188-213-241.us-east-2.compute.amazonaws.com",
+  "http://ec2-18-225-6-113.us-east-2.compute.amazonaws.com"
+];
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.static("dist"));
 
-app.post('/users', middleware.itemLookup, async (req, res) => {
+app.post("/users", middleware.itemLookup, async (req, res) => {
   const item = req.body.item;
   if (!req.cookies.user_session) {
     const sessionId = helpers.randomStringifiedNumberOfLength(8);
     await db.createUser(Number(sessionId));
     const user = await db.getUser(sessionId);
-    await db.recordView(user.id, item.id);    
-    res.cookie('user_session', Number(sessionId)).status(201).send();
+    await db.recordView(user.id, item.id);
+    res
+      .cookie("user_session", Number(sessionId))
+      .status(201)
+      .send();
   } else {
     const user = await db.getUser(req.cookies.user_session);
     await db.recordView(user.id, item.id);
@@ -31,7 +53,7 @@ app.post('/users', middleware.itemLookup, async (req, res) => {
   }
 });
 
-app.get('/carousels', middleware.itemLookup, async (req, res) => {
+app.get("/carousels", middleware.itemLookup, async (req, res) => {
   const item = req.body.item;
   const carousels = {};
 
