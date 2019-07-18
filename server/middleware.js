4201b572ffb85b9ae7c7f3ea2cc0ed8aa9ce0b14
Mitchell Dill <malexanderdill@gmail.com>
2019-07-17 19:21:22 -0500

    Implement corsOptions to allow cookie-auth to work on proxy

--- server/middleware.js
+++ server/middleware.js
@@ -1,10 +1,10 @@
 const db = require("../database/index.js");
 
 const whitelist = [
-  "http://fec-proxy.us-east-1.elasticbeanstalk.com/",
-  "http://lowesproxy-env.6tim4uzsty.us-east-2.elasticbeanstalk.com/",
+  "http://fec-proxy.us-east-1.elasticbeanstalk.com",
+  "http://lowesproxy-env.6tim4uzsty.us-east-2.elasticbeanstalk.com",
   "http://localhost:3000",
-  "http://fec-lowes-carousel.us-east-2.elasticbeanstalk.com/",
+  "http://fec-lowes-carousel.us-east-2.elasticbeanstalk.com",
   "http://ec2-18-188-213-241.us-east-2.compute.amazonaws.com",
   "http://ec2-18-225-6-113.us-east-2.compute.amazonaws.com"
 ];
