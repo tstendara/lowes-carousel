23a5e5aa1ebb64eecb5c212d46827efd1f482949
Mitchell Dill <malexanderdill@gmail.com>
2019-07-19 22:46:22 -0500

    Add proxy to whitelist

--- server/middleware.js
+++ server/middleware.js
@@ -2,13 +2,14 @@ const db = require("../database/index.js");
 
 const whitelist = [
   "http://fec-proxy.us-east-1.elasticbeanstalk.com",
+  "http://fec-lowes-proxy.us-east-2.elasticbeanstalk.com/",
   "http://lowesproxy-env.6tim4uzsty.us-east-2.elasticbeanstalk.com",
-  "http://localhost:3000",
-  "http://localhost:3050",
   "http://fec-lowes-carousel.us-east-2.elasticbeanstalk.com",
   "http://ec2-18-188-213-241.us-east-2.compute.amazonaws.com",
   "http://ec2-18-225-6-113.us-east-2.compute.amazonaws.com",
-  "http://lowesproductoverview-env.mk2qecy2ne.us-east-2.elasticbeanstalk.com/"
+  "http://lowesproductoverview-env.mk2qecy2ne.us-east-2.elasticbeanstalk.com/",
+  "http://localhost:3000",
+  "http://localhost:3050"
 ];
 
 const corsOptions = {
